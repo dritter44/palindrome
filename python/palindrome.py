cabbfc8ce3a38c96925eca5fd091d39e9a27d11f
dritter44 <dreither44@gmail.com>
2022-05-30 17:02:00 -0500

    test cases run individually works

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,22 @@
 def palindrome(word):
+    input_word = ""
+    if type(word) is int:
+        input_word = str(word)
+    else:
+        input_word = word
+    #print(type(input_word))
+    cln_str = ''
+    for letter in input_word:
+        if letter.isalnum():
+            cln_str = cln_str + letter
+    cln_str1 = cln_str.lower()
+    #return cln_str1
+    if (len(cln_str1) == 2 or len(cln_str1) == 3) and (cln_str1[0] == cln_str1[len(cln_str1)-1]):
+        return True
+    if (len(cln_str1) >= 4) and (cln_str1[0] == cln_str1[len(cln_str1)-1]):
+        return palindrome(cln_str1[1:len(cln_str1)-1])
+    else:
+        return False
+
     # Write code here
+print(palindrome('A man, a plan, a canal -- Panama'))
